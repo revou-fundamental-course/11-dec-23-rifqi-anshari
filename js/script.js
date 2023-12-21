a9462ff58d585820ad591ad4e887420482ee730f
rifqi-anshari <rifqianshari22@gmail.com>
2023-12-21 20:59:08 +0800

    ke4

--- js/script.js
+++ js/script.js
@@ -1,3 +1,34 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Fungsi untuk menampilkan slide berikutnya atau sebelumnya
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Fungsi untuk menampilkan slide tertentu
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+// Fungsi untuk menampilkan slide
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+      setTimeout(showSlides, 2000); // Waktu pergantian slide dalam milidetik
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
 function submit(){ 
     const date = new Date();
     var name=document.getElementById("name").value; 
